82d0f8801ac1c78bc46d844f519274d5e0fab118
Abinash Sinha <abinashsinha330@gmail.com>
2021-06-24 02:41:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
+import time
 import argparse
+from tqdm import tqdm
+
+from model import Model
 import utils
 
 
@@ -26,14 +30,14 @@ with open(os.path.join(args.dataset + '_' + args.train_dir, 'args.txt'), 'w') as
 f.close()
 
 # prepare sequences of items for each user as per given user-item interactions
-student_chapters_dict = utils.get_student_chapters_interactions(args.dataset)
+student_chapters_dict = utils.get_student_chapters_sequences(args.dataset)
 
 # calculate statistics related to dataset
 num_students, num_chapters = utils.calculate_data_stats(student_chapters_dict)
 
 # split data into training, validation and test sets
 dataset = utils.train_valid_test_split(student_chapters_dict)
-[student_train, student_valid, student_test] = dataset
+[student_train, student_valid, user_test] = dataset
 
 
 num_batches = len(student_train) / args.batch_size
@@ -41,3 +45,44 @@ cc = 0.0
 for u in student_train:
     cc += len(student_train[u])
 print('Average sequence length in training set: %.2f' % (cc / len(student_train)))
+
+
+sampler = utils.Sampler(student_train, num_students, num_chapters,
+                        batch_size=args.batch_size, maxlen=args.max_len, n_workers=3)
+model = Model(num_students, num_chapters, args)
+# sess.run(tf.initialize_all_variables())
+
+# code to run training given the number of epochs
+T = 0.0
+t0 = time.time()
+
+try:
+    for epoch in range(1, args.num_epochs + 1):
+
+        for step in tqdm(range(num_batches), total=num_batches, ncols=70, leave=False, unit='b'):
+            u, seq, pos, neg = sampler.next_batch()
+            auc, loss, _ = sess.run([model.auc, model.loss, model.train_op],
+                                    {model.u: u, model.input_seq: seq, model.pos: pos, model.neg: neg,
+                                     model.is_training: True})
+
+        if epoch % 20 == 0:
+            t1 = time.time() - t0
+            T += t1
+            print('Evaluating')
+            t_test = evaluate(model, dataset, args, sess)
+            t_valid = evaluate_valid(model, dataset, args, sess)
+            print('')
+            print('epoch:%d, time: %f(s), valid (NDCG@10: %.4f, HR@10: %.4f), test (NDCG@10: %.4f, HR@10: %.4f)' % (
+                epoch, T, t_valid[0], t_valid[1], t_test[0], t_test[1]))
+
+            f.write(str(t_valid) + ' ' + str(t_test) + '\n')
+            f.flush()
+            t0 = time.time()
+except:
+    sampler.close()
+    f.close()
+    exit(1)
+
+f.close()
+sampler.close()
+print("Done")
